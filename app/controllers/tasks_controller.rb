167b31d71b15f1dff71b25d0d64db116625773b1
tomoaki_narukawa <tomo111911111@gmail.com>
2022-12-01 17:42:22 +0900

    tasksコントローラー作成,tasks/index.htmlの一覧画面を作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice:"登録しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+    set_task
+  end
+
+  def edit
+    set_task
+  end
+
+  def update
+    set_task
+    if @task.update(task_params)
+      redirect_to tasks_path, notice:"更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_task
+    if @task.destroy
+      redirect_to tasks_path, notice:"削除しました"
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    @task = params[:task].permit(:task_name, :task_detail)
+  end
+end
