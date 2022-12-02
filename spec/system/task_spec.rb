16caab17a210f81401b3d39fd84aefad429268b1
tomoaki_narukawa <tomo111911111@gmail.com>
2022-12-02 17:39:42 +0900

    並び替え降順System specパス確認

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-  end
+  let!(:task){FactoryBot.create(:task)}
+  let!(:task2){FactoryBot.create(:second_task)}
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -11,25 +9,38 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in "タスク名", with: 'updated_1'
         fill_in "内容", with: 'updated_2'
-        click_button "Create Task"
+        click_button "登録する"
         expect(page).to have_content 'updated_1'
       end
     end
   end
+
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task)
+        task
         visit tasks_path
         expect(page).to have_content 'task_name'
       end
     end
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        @task1 = task
+        @task2 = task2
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list.first).to have_content '2個'
+        binding.irb
+      end
+    end
   end
+
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:second_task)
-        visit task_path(task)
+        task2
+        visit task_path(task2)
         expect(page).to have_content '2個'
        end
      end
