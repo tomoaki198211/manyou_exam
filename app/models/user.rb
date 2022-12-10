f4cccd5c7aa75a5768ef9829a8f92142f5e8bc2f
tomoaki_narukawa <tomo111911111@gmail.com>
2022-12-10 12:47:09 +0900

    userのバリデーションを修正マックス8となっていた為

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   has_secure_password
-  validates :password, length: { maximum:8}
+  validates :password, length: { minimum: 6}
 
   private
 
